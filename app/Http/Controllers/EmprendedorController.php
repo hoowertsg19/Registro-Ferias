3d2e040190d19766f9196b6d283b56e9738dbf6b
hoowertsg19 <hjgross@uamv.edu.ni>
2025-05-13 00:07:33 -0600

    Controladores funcionales y views para  para ferias y emprendedores

---
+++ app/Http/Controllers/EmprendedorController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Emprendedor;
+use Illuminate\Http\Request;
+
+class EmprendedorController extends Controller
+{
+    public function index()
+    {
+        $emprendedores = Emprendedor::all();
+        return view('emprendedores.index', compact('emprendedores'));
+    }
+
+    public function create()
+    {
+        return view('emprendedores.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'telefono' => 'required',
+            'rubro' => 'required',
+        ]);
+
+        Emprendedor::create($request->all());
+
+        return redirect()->route('emprendedores.index');
+    }
+
+    public function edit(Emprendedor $emprendedor)
+    {
+        return view('emprendedores.edit', compact('emprendedor'));
+    }
+
+    public function update(Request $request, Emprendedor $emprendedor)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'telefono' => 'required',
+            'rubro' => 'required',
+        ]);
+
+        $emprendedor->update($request->all());
+
+        return redirect()->route('emprendedores.index');
+    }
+
+    public function destroy(Emprendedor $emprendedor)
+    {
+        $emprendedor->delete();
+        return redirect()->route('emprendedores.index');
+    }
+
+    public function show(Emprendedor $emprendedor)
+    {
+        return view('emprendedores.show', compact('emprendedor'));
+    }
+}
---
+++ app/Http/Controllers/FeriaController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Feria;
+use App\Models\Emprendedor;
+use Illuminate\Http\Request;
+
+class FeriaController extends Controller
+{
+    public function index()
+    {
+        $ferias = Feria::with('emprendedores')->get();
+        return view('ferias.index', compact('ferias'));
+    }
+
+    public function create()
+    {
+        $emprendedores = Emprendedor::all();
+        return view('ferias.create', compact('emprendedores'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'fecha' => 'required|date',
+            'lugar' => 'required',
+            'descripcion' => 'required',
+        ]);
+
+        $feria = Feria::create($request->only(['nombre', 'fecha', 'lugar', 'descripcion']));
+        $feria->emprendedores()->sync($request->emprendedores ?? []);
+
+        return redirect()->route('ferias.index');
+    }
+
+    public function edit(Feria $feria)
+    {
+        $emprendedores = Emprendedor::all();
+        $feriaEmps = $feria->emprendedores->pluck('id')->toArray();
+        return view('ferias.edit', compact('feria', 'emprendedores', 'feriaEmps'));
+    }
+
+    public function update(Request $request, Feria $feria)
+    {
+        $request->validate([
+            'nombre' => 'required',
+            'fecha' => 'required|date',
+            'lugar' => 'required',
+            'descripcion' => 'required',
+        ]);
+
+        $feria->update($request->only(['nombre', 'fecha', 'lugar', 'descripcion']));
+        $feria->emprendedores()->sync($request->emprendedores ?? []);
+
+        return redirect()->route('ferias.index');
+    }
+
+    public function destroy(Feria $feria)
+    {
+        $feria->delete();
+        return redirect()->route('ferias.index');
+    }
+
+    public function show(Feria $feria)
+    {
+        return view('ferias.show', compact('feria'));
+    }
+}
